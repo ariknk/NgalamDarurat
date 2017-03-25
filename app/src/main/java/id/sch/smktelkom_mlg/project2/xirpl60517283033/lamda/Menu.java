14fd1156bb5024cc0ae31bdc29d20186967f9b12
Arik Nur Khoiriyah <ariknurkhoiriyah@gmail.com>
2017-03-25 20:49:54 +0700

    Menu

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl60517283033/lamda/Menu.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl60517283033/lamda/Menu.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project2.xirpl60517283033.lamda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +11,49 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        findViewById(R.id.rs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, RumahSakit.class);
+                Menu.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.polisi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Polisi.class);
+                Menu.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.taksi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Taksi.class);
+                Menu.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.pemadam).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Pemadam.class);
+                Menu.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.wiuwiu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Ambulance.class);
+                Menu.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.angkot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Angkot.class);
+                Menu.this.startActivity(intent);
+            }
+        });
     }
 }
