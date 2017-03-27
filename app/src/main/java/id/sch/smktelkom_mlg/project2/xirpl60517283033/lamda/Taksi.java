4cf145f0b297cfc0f30553759f5f261375a817c8
Arik Nur Khoiriyah <ariknurkhoiriyah@gmail.com>
2017-03-27 17:14:33 +0700

    telp taxi

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl60517283033/lamda/Taksi.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl60517283033/lamda/Taksi.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.project2.xirpl60517283033.lamda;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Taksi extends AppCompatActivity {
 
@@ -9,5 +12,54 @@ public class Taksi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taksi);
+
+        findViewById(R.id.citra).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(" 0341490555");
+            }
+
+            private void dialPhoneNumber(String phoneNumber) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+            }
+        });
+        findViewById(R.id.argo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(" 0341488888");
+            }
+
+            private void dialPhoneNumber(String phoneNumber) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+            }
+        });
+        findViewById(R.id.manda).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(" 0341474747");
+            }
+
+            private void dialPhoneNumber(String phoneNumber) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+            }
+        });
+        findViewById(R.id.bima).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(" 0341717171");
+            }
+
+            private void dialPhoneNumber(String phoneNumber) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+            }
+        });
     }
 }
